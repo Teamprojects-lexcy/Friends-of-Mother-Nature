56bbf8e0de3b2fd8c5cc3e9c5bd935b34f52a01e
Alex Mwaura <alecsmwaura@gmail.com>
2023-05-08 00:26:40 +0300

    .

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Menu Burger homepage
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.navlinks');
+    const navlinks = document.querySelectorAll('.navlinks li');
+  
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      navlinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = ''
+        }else{
+          link.style.animation = `navlinkFade 0.5s ease forward ${index / 7 + 4 }s`;
+        } 
+      });
+      burger.classList.toggle('toggles');
+    });
+  }
+  navSlide();
